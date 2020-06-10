80b863e543847804f093ef2c8c020c76b58b9f85
Megan Gross <144.mdgross@gmail.com>
2020-06-10 15:54:52 -0600

    get the latest and only the latest answer to each question.

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,30 +3,40 @@ class LessonsController < ApplicationController
   def show
     @lesson = Lesson.find(allow_params[:id])
     @version_user = VersionUser.where(user_id: allow_params[:user])
+
+    # ensure the user has been assigned to the lesson
     if !@version_user.any? {|h| h[:version] == @lesson[:version]}
-      # TODO: could throw exception and let middleware catch once set up.
-      render json: {error: "Not Found", status: '404'}
+      render json: { error: "Not Found", status: '404' }
       return
     end
 
     @questions = Question.where(lesson_id: @lesson[:id])
-    @uids = Array.new
-    # now get the uid instead
+
+    # now get the uids of each question to properly track answers.
+    @uids = []
     @questions.each do |q|
       @uids.push(q[:uid])
     end
 
-    @question_ids = Question.where(:uid => @uids).pluck(:id)
-    @answers = Answer.where(:question_id =>@question_ids).where(:user_id => @version_user[0][:user_id]).order('id DESC')
+    @all_questions = Question.where(:uid => @uids)
+    @answers = Answer.where(:question_id => @all_questions.pluck(:id)).where(:user_id => @version_user[0][:user_id]).order('id ASC')
 
-    if @answers.empty?
-      @answers = nil
+    @final_answers = {}
+
+    # loop over each potential question there could be an answer for and collect most recent answers.
+    @all_questions.each do |q|
+      @answers.each do |a|
+        if  a[:question_id].equal? q[:id]
+          # NOTE. Answers are sorted by ID, so the most recent id will overwrite this key making the most recent answer display.
+          @final_answers[q[:uid]] = a
+        end
+      end
     end
 
     render json: {
         lesson: @lesson[:content],
         questions: @questions.to_json(:except => [:correct_answer]),
-        answers: @answers[0].to_json(:only => [:answer, :user_id, :question_id])
+        answers: @final_answers,
     }
 
   rescue ActiveRecord::RecordNotFound
